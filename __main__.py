49f0086186a224341ea4fd2fa83723f54e8d7b3e
Raphael <raphaelstjean@yahoo.ca>
2022-01-03 01:52:18 -0500

    moving the generated log file in the rigth folder

--- __main__.py
+++ __main__.py
@@ -15,9 +15,10 @@ load_dotenv()
 
 #logging
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('discord')
+logger = logging.getLogger("discord")
 logger.setLevel(logging.ERROR)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "timed_bot.log")
+handler = logging.FileHandler(filename=file, encoding="utf-8", mode="w")
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
