7bb8ab3ac7fe56b115f8e5924fdf6a88e4c7c223
Raphael <raphaelstjean@yahoo.ca>
2022-10-22 00:39:15 -0400

    fix database file not at the good place

--- constant.py
+++ constant.py
@@ -16,7 +16,7 @@ datetime_strptime = "%Y-%m-%d %H:%M:%S"
 
 LOG_DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "logs")
 
-default_database = "time_role.db"
+default_database = os.path.join(os.path.dirname(os.path.realpath(__file__)), "time_role.db")
 DATABASE = os.getenv("DATABASE_FILE")
 if DATABASE is None:
     DATABASE = default_database
@@ -26,7 +26,7 @@ LOCAL_TIME_ZONE = pytz.timezone(os.getenv("TIMEZONE"))
 if LOCAL_TIME_ZONE is None:
     LOCAL_TIME_ZONE = default_timezone
 
-default_backup = "backups/"
+default_backup = os.path.join(os.path.dirname(os.path.realpath(__file__)), "backups")
 BACKUP_DIR = os.getenv("BACKUP_DIR")
 if BACKUP_DIR is None:
     BACKUP_DIR = default_backup
